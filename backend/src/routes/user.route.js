a2b0d479528b8968696599abe87a807dfbdffa6e
Austine Mahugu <austinemahugu8@gmail.com>
2026-07-09 12:22:14 +0300

    Feature add auth APIs:(register, login, logout)

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import {logoutUser, registerUser, loginUser} from '../controllers/user.controller.js';
+
+
+const router = Router();
+
+router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
+router.route('/logout').post(logoutUser);
+ 
+export default router;
